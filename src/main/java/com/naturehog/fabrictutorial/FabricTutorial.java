c88275cec522a54653e9e81f5610b6f5f0a61ab5
NatureHog <naturehog@disroot.org>
2021-04-10 17:24:22 +0200

    initial setup
    Signed-off-by: NatureHog <naturehog@disroot.org>

---
+++ src/main/java/com/naturehog/fabrictutorial/FabricTutorial.java
@@ -0,0 +1,12 @@
+package com.naturehog.fabrictutorial;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricTutorial implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
