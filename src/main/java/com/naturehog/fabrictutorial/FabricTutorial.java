3e4dec45224d576094b737a6c9618270ce1beefc
NatureHog <naturehog@disroot.org>
2021-04-10 17:32:00 +0200

    i forgot something...
    Signed-off-by: NatureHog <naturehog@disroot.org>

--- src/main/java/com/naturehog/fabrictutorial/FabricTutorial.java
+++ src/main/java/com/naturehog/fabrictutorial/FabricTutorial.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 
 public class FabricTutorial implements ModInitializer {
 
+    public static final String MOD_ID = "nh_fabrictutorial";
+
     @Override
     public void onInitialize() {
 
